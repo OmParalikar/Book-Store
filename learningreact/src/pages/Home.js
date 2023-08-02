d535f2b82ce9e17f68be064d3de9323e14abd215
OmParalikar <omparalikar45@gmail.com>
2023-08-02 18:37:52 +0530

    Initial commit

--- learningreact/src/pages/Home.js
+++
@@ -1,3 +0,0 @@
-export const Home=(props)=>{
-    return <h1>This is home page and user is:{props.username}</h1>;
-};
--- learningreact/src/pages/Profile.js
+++
@@ -1,9 +0,0 @@
-import {ChangeProfile} from "../components/ChangeProfile";
-export const Profile =(props)=>{
-    return (
-        <div>
-            Profile, user is: {props.username}
-            <ChangeProfile setUsername={props.setUsername}/>
-        </div>
-    );
-};
